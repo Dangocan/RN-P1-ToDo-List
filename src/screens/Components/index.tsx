3cd4a22537aea92cbe96f806df82d9f2e7531186
Dangocan <danilogc1999@gmail.com>
2023-12-17 00:20:10 -0300

    feat: adds new components to barrel

--- src/screens/Components/index.tsx
+++ src/screens/Components/index.tsx
@@ -1,3 +1,15 @@
+import TaskItem from "./TaskItem";
+import AddTaskInput from "./AddTaskInput";
 import LogoImageSVG from "./LogoImageSVG";
+import PlusImageSVG from "./PlusImageSVG";
+import EmptyListImageSVG from "./EmptyListImageSVG";
+import EmptyListComponent from "./EmptyListComponent";
 
-export { LogoImageSVG };
+export {
+  TaskItem,
+  AddTaskInput,
+  LogoImageSVG,
+  PlusImageSVG,
+  EmptyListImageSVG,
+  EmptyListComponent,
+};
